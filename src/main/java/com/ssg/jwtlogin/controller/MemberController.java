cf2f5b91dbebbf4a268d5a32a28af807225d5edf
heeezni <hjin3456@naver.com>
2025-09-04 14:59:16 +0900

    ✨ feat(login): 사용자 로그인 백엔드 처리 로직 구현 (진행중)
    - SecurityConfig에 PasswordEncoder (BCrypt)를 등록하여 비밀번호 암호화 처리
    - MemberController에 /member/login POST 엔드포인트를 추가하여 사용자 로그인 요청을 처리하는 로직 구현
    - login.html에 jQuery와 AJAX 호출을 추가하여, 사용자가 입력한 아이디와 비밀번호를 서버로 전송하는 프론트엔드 로직을 구현

--- src/main/java/com/ssg/jwtlogin/controller/MemberController.java
+++ src/main/java/com/ssg/jwtlogin/controller/MemberController.java
@@ -1,8 +1,35 @@
 package com.ssg.jwtlogin.controller;
 
+import com.ssg.jwtlogin.domain.CustomUserDetails;
+import com.ssg.jwtlogin.model.member.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RequiredArgsConstructor
+@Slf4j
 @Controller
 public class MemberController {
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
+    @PostMapping("/member/login")
+    public ResponseEntity<?> login(@RequestParam("id") String username, @RequestParam("pwd") String password) {
+        log.debug("로그인 요청받음");
+
+        // 유저 꺼내기
+        CustomUserDetails customUserDetails = (CustomUserDetails) customUserDetailsService.loadUserByUsername(username);
+
+        // 추출한 유저의 비밀번호 - 파라미터로 넘어온 유저의 비번 비교
+        if (customUserDetails==null && passwordEncoder.matches(password, customUserDetails.getPassword())) {
+            // 로그인 성공하였으므로, JWT 발급
+
+        }
+
+        return null;
+    }
 }
--- src/main/java/com/ssg/jwtlogin/security/SecurityConfig.java
+++ src/main/java/com/ssg/jwtlogin/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -13,10 +15,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    // 현재 DB에 들어간 비밀번호는 BCrypt 알고리즘이므로, Security에게 같은 알고리즘을 사용하도록 알려주기
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
             .csrf(AbstractHttpConfigurer::disable).build();
     }
+
 }
